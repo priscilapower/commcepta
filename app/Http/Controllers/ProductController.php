3c718bc253b30b80f4cd4bbecca055bf04bc23de
Priscila Power <priscilapower92@gmail.com>
2019-05-08 22:42:55 -0300

    incluindo o template e ajustando o crud de produto

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,93 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
+use App\Product;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display a listing of the products
+     *
+     * @param  \App\Product  $model
+     * @return \Illuminate\View\View
+     */
+    public function index(Product $model)
+    {
+        return view('product.index', ['products' => $model->paginate(15)]);
+    }
+
+    /**
+     * Show the form for creating a new product
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('product.create');
+    }
+
+    /**
+     * Store a newly created product in storage
+     *
+     * @param  \App\Http\Requests\ProductRequest  $request
+     * @param  \App\Product  $model
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(ProductRequest $request, Product $model)
+    {
+        $model->create($request->all());
+
+        return redirect()->route('product.index')->withStatus(__('Product successfully created.'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Product $product
+     * @return \Illuminate\View\View
+     */
+    public function show(Product $product)
+    {
+        return view('product.view', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified product
+     *
+     * @param  \App\Product $product
+     * @return \Illuminate\View\View
+     */
+    public function edit(Product $product)
+    {
+        return view('product.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified product in storage
+     *
+     * @param ProductRequest $request
+     * @param Product $product
+     * @return mixed
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+        $product->update($request->all());
+
+        return redirect()->route('product.index')->withStatus(__('Product successfully updated.'));
+    }
+
+    /**
+     * Remove the specified product from storage
+     *
+     * @param Product $product
+     * @return mixed
+     * @throws \Exception
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return redirect()->route('product.index')->withStatus(__('Product successfully deleted.'));
+    }
 }
--- app/Http/Controllers/SellerController.php
+++ app/Http/Controllers/SellerController.php
@@ -2,9 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Seller;
 use Illuminate\Http\Request;
 
 class SellerController extends Controller
 {
-    //
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+    {
+        return view('seller.index', ['sellers' => Seller::all()]);
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('seller.create');
+    }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Throwable
+     */
+    public function store(Request $request)
+    {
+        Seller::saveOrFail($request->all());
+        return view('seller.index', ['sellers' => Seller::all()]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show($id)
+    {
+        return view('seller.view', ['seller' => Seller::findOrFail($id)]);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function edit($id)
+    {
+        return view('seller.edit', ['seller' => Seller::findOrFail($id)]);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function update(Request $request, $id)
+    {
+        Seller::update($request->all(), $id);
+        return view('seller.index', ['sellers' => Seller::all()]);
+    }
+
+    /**
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function destroy($id)
+    {
+        Seller::destroy($id);
+        return view('seller.index', ['sellers' => Seller::all()]);
+    }
 }
